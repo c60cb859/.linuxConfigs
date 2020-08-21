5afb0692bab122faae0e9650b22657f270921470
Theis <theisc@pm.me>
2020-08-21 17:23:51 +0200

    added a bunch more configs

--- setupConfig.sh
+++ setupConfig.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-ln -s ~/.linuxConfigs/.bashrc ~/
-ln -s ~/.linuxConfigs/.tmux.conf ~/
+# Tmux
+rm /home/$USER/.tmux.conf
+ln -s /home/$USER/.config/.linuxConfigs/tmux.conf /home/$USER/.tmux.conf
 
-mkdir ~/bin
+# Termite
+mkdir -p /home/$USER/.config/termite
+rm /home/$USER/.config/termite/config
+ln -s /home/$USER/.config/.linuxConfigs/termite.conf /home/$USER/.config/termite/config
+
+# zsh
+rm /home/$USER/.zshrc
+ln -s /home/$USER/.config/.linuxConfigs/zshrc /home/$USER/.zshrc
+
+# Redshift
+mkdir -p /home/$USER/.config/redshift
+rm /home/$USER/.config/redshift/redshift.conf
+ln -s /home/$USER/.config/.linuxConfigs/redshift.conf /home/$USER/.config/redshift/redshift.conf
+
+#mkdir ~/bin
